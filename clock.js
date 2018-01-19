82c89959abb7d86aa77815be1e4b40aca231fd7a
SheilaWaples <sheilawaples78@gmail.com>
2018-01-19 14:46:32 -0500

    changed background-color

--- clock.js
+++ clock.js
@@ -1,25 +1,5 @@
-function clock(){
-	var time=new Date();
-	var hours= time.getHours();
-	var minutes= time.getMinutes();
-	var seconds= time.getSeconds();
-	minutes= checkTime(0);
-	seconds= checkTime();
+var time = new Date ();
+console.log(time.getHours() % 12);
+console.log(time.getMinutes());
+console.log(time.getSeconds());
 
-}
- if (hours <10){
-	hours="0" + hours;
-
-}
-
-if (minutes <10){
-	minutes="0" + minutes;
-}
-
-if (seconds <10){
-
-	seconds= "0" + seconds;
-}
-var currentTime= hour + '.' + minutes + '.' + seconds;
-var clock= document.getElementById('clock');
-// SetInterval('clock', 1000);
