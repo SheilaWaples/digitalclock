9745b08a143073575552a49e4956584d5b587e04
SheilaWaples <sheilawaples78@gmail.com>
2018-01-19 15:48:52 -0500

    styled h1

--- clock.js
+++ clock.js
@@ -1,5 +1,26 @@
-var time = new Date ();
-console.log(time.getHours() % 12);
-console.log(time.getMinutes());
-console.log(time.getSeconds());
+function myClock(){
+	var time= new Date();
+	var hours= time.getHours();
+	var minutes= time.getMinutes();
+	var seconds= time.getSeonds();
 
+
+
+if (hours<10){
+	'0' + hours
+}
+
+if (minutes<10){
+	'0' + minutes
+}
+
+if (seconds<10){
+	'0' + seconds
+}
+
+if (hours.length <2){
+
+	hours='0' + hours;
+}
+
+}
